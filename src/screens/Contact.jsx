7ec664c5aac06ff0a6565190da99024aab172dba
ibadd718 <ibadd718@gmail.com>
2024-08-03 08:08:25 -0700

    Updated contact button

--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -90,11 +90,8 @@ const Contact = () => {
               ></textarea>
             </div>
 
-            <div className="flex justify-center">
-              <button
-                type="submit"
-                className="px-6 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              >
+            <div className="flex ">
+              <button className="bg-[#D6B8FF] font-semibold text-white py-3 px-16 text-xl transition-transform transform hover:scale-105 hover:bg-[#B09CFF]">
                 Enviar
               </button>
             </div>
