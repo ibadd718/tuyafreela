890c45ae4c17e6e70d3004f02f8386c9f6d3c3a9
MMohsinHussain <99159763+MMohsinHussain@users.noreply.github.com>
2024-08-06 00:53:46 +0500

    updated ui

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import { CiMail, CiPhone } from "react-icons/ci";
 import Logo from "../../assets/Logo.png";
+import { IoLogoInstagram } from "react-icons/io";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
 
 const Footer = () => {
   return (
     <footer className="bg-[#363636] p-10 text-white">
       <div className="container mx-auto px-4 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <aside className="flex flex-col items-start">
             <img src={Logo} className="w-32 mb-4" alt="Company Logo" />
             <nav className="flex flex-col">
@@ -49,6 +53,22 @@ const Footer = () => {
             <Link to="/faq" className="link link-hover mb-2">
               F.A.Q
             </Link>
+          </nav>
+          <nav className="flex flex-col">
+            <h1 className="footer-title text-[#D6B8FF] text-lg mb-4">
+              Siga - nos
+            </h1>
+            <div className="flex  gap-5  text-black ">
+              <Link to="/about" className="link bg-white link-hover p-2 rounded-full mb-2">
+                <FaFacebookF size={26} />
+              </Link>
+              <Link to="/about" className="link bg-white link-hover p-2 rounded-full mb-2">
+                <IoLogoInstagram size={26} />
+              </Link>
+              <Link to="/faq" className="link bg-white link-hover p-2 rounded-full mb-2">
+                <FaTwitter size={26} />
+              </Link>
+            </div>
           </nav>
         </div>
         <div className="text-center mt-8">
